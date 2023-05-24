74e4d33b4ed52ba93c66b256ebb3b6dbbfe02a89
Quant - UFABC Finance <132862124+QuantUFABCFinance1@users.noreply.github.com>
2023-05-23 21:53:26 -0300

    Update Coletania.py

--- Coletania.py
+++ Coletania.py
@@ -1,6 +1,7 @@
 #Importar Scrapp e bibliotecas
 from ws_BloomBerg import BloomBerg_WS
 from ws_ValorEconomico import ValorEconomico_WS
+from ws_Investing import Investing_WS
 #from ws_investing import Investing_WS
 import pandas as pd
 
@@ -10,10 +11,10 @@ def coletania():
         #Chamar Scrapping
     bb = BloomBerg_WS()
     ve = ValorEconomico_WS()
-    #in = Investing_WS()
+    inv = Investing_WS()
 
-    coletania = pd.merge(bb, ve, 'outer')
-    #coletania = pd.merge(bb, ve, in, 'outer')
+    #coletania = pd.merge(bb, ve, 'outer')
+    coletania = pd.merge(bb, ve, inv, 'outer')
     print('Noticias consolidadas.')
 
     noticiasAletorias = coletania.sample(5)
